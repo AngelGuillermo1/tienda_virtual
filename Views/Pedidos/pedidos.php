032fca41620a2cfb84efc003c87c7e20bd88d054
angelguillermo1 <147960757+AngelGuillermo1@users.noreply.github.com>
2024-10-07 20:25:51 -0600

    pedidos

--- Views/Pedidos/pedidos.php
+++ Views/Pedidos/pedidos.php
@@ -1,19 +1,16 @@
 <?php 
     headerAdmin($data); 
-    getModal('modalCategorias',$data);
+    //getModal('modalProductos',$data);
 ?>
+    <div id="divModal"></div>
     <main class="app-content">
       <div class="app-title">
         <div>
-            <h1><i class="fas fa-box-tissue"></i> <?= $data['page_title'] ?>
-              <?php if($_SESSION['permisosMod']['w']){ ?>
-                <button class="btn btn-primary" type="button" onclick="openModal();" ><i class="fas fa-plus-circle"></i> Nuevo</button>
-              <?php } ?> 
-            </h1>
+            <h1><i class="fas fa-box"></i> <?= $data['page_title'] ?></h1>
         </div>
         <ul class="app-breadcrumb breadcrumb">
           <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-          <li class="breadcrumb-item"><a href="<?= base_url(); ?>/categorias"><?= $data['page_title'] ?></a></li>
+          <li class="breadcrumb-item"><a href="<?= base_url(); ?>/pedidos"><?= $data['page_title'] ?></a></li>
         </ul>
       </div>
         <div class="row">
@@ -21,13 +18,15 @@
               <div class="tile">
                 <div class="tile-body">
                   <div class="table-responsive">
-                    <table class="table table-hover table-bordered" id="tableCategorias">
+                    <table class="table table-hover table-bordered" id="tablePedidos">
                       <thead>
                         <tr>
                           <th>ID</th>
-                          <th>Nombre</th>
-                          <th>Descripción</th>
-                          <th>Status</th>
+                          <th>Ref. / Transacción</th>
+                          <th>Fecha</th>
+                          <th>Monto</th>
+                          <th>Tipo pago</th>
+                          <th>Estado</th>
                           <th>Acciones</th>
                         </tr>
                       </thead>
